777129d01f2e87d664aed08e1018811086c44854
Justin Niessner <CrshOverride@gmail.com>
2020-05-26 15:17:36 -0400

    Bump minor beta version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@
 from setuptools import setup, find_packages  # noqa: H301
 
 NAME = "adzerk-decision-sdk"
-VERSION = "1.0.0-beta.2"
+VERSION = "1.0.0-beta.3"
 # To install the library, run the following
 #
 # python setup.py install
